24f9039806ca1931dc350333f4bff84356e11ab8
Naveed Khan <naveealikhan@outlook.com>
2017-01-06 22:41:49 +0500

    context added

---
+++ SMS.DAL/SmsContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace SMS.DAL
+{
+    public class SmsContext : DbContext
+    {
+        protected SmsContext() : base()
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
